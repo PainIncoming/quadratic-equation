21c8452d72c05b4bd3aed6be1a271b8b36d28c02
PainIncoming <galko3.v@gmail.com>
2018-09-14 01:28:32 +0300

    Implement solveEquation function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
-}
+  let [a, b, c] = equation
+      .match( /([-+]|^) ?\d+/g )
+      .map( item => parseFloat(item.replace(' ', '')) );
+
+  let D = Math.pow(b, 2) - 4 * a * c;
+
+  let x1 = Math.round((-b + Math.sqrt(D)) / (2 * a)),
+      x2 = Math.round((-b - Math.sqrt(D)) / (2 * a));
+
+  return [x1, x2].sort((a, b) => a - b);
+};
